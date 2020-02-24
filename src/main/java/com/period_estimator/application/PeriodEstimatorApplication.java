f3a1cbea7812d9fbdf0efb2c969950fc74c2f8d4
Phetho Malope <pmalope@student.wethinkcode.co.za>
2020-02-24 09:33:35 +0200

    initial commit

---
+++ src/main/java/com/period_estimator/application/PeriodEstimatorApplication.java
@@ -0,0 +1,13 @@
+package com.period_estimator.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeriodEstimatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeriodEstimatorApplication.class, args);
+	}
+
+}
